5d0caa3b0f391687f452ef34c1ea95f27c77a581
Dae-Iddings <115945028+Dae-Iddings@users.noreply.github.com>
2022-12-28 22:16:39 -0600

    shows markers on gameboard

--- myScript.js
+++ myScript.js
@@ -24,4 +24,24 @@ const player = (playerName) => {
     let newName = playerName;
     return {newName}
 }
-
+function renderGameBoard() {
+    const renderSpot1 = document.querySelector('#spot1');
+    const renderSpot2 = document.querySelector('#spot2');
+    const renderSpot3 = document.querySelector('#spot3');
+    const renderSpot4 = document.querySelector('#spot4');
+    const renderSpot5 = document.querySelector('#spot5');
+    const renderSpot6 = document.querySelector('#spot6');
+    const renderSpot7 = document.querySelector('#spot7');
+    const renderSpot8 = document.querySelector('#spot8');
+    const renderSpot9 = document.querySelector('#spot9');
+    renderSpot1.textContent = gameBoard.spot1('x');
+    renderSpot2.textContent = gameBoard.spot2('o');
+    renderSpot3.textContent = gameBoard.spot3('x');
+    renderSpot4.textContent = gameBoard.spot4('o');
+    renderSpot5.textContent = gameBoard.spot5('x');
+    renderSpot6.textContent = gameBoard.spot6('o');
+    renderSpot7.textContent = gameBoard.spot7('x');
+    renderSpot8.textContent = gameBoard.spot8('o');
+    renderSpot9.textContent = gameBoard.spot9('x');
+}
+renderGameBoard();
